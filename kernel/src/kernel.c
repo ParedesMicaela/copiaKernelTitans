36f9c65ce4b509a54ba851724e2fefa162ce5ec2
Paredes Micaela <miparedes@frba.utn.edu.ar>
2023-09-12 17:03:06 -0300

    mashup con  premain

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,7 +1,6 @@
 #include "kernel.h"
 
-//Variables Globale//
-
+//================================================= Varibales Globales =====================================================================
 t_log* kernel_logger;
 t_config* config;
 int server_fd;
@@ -11,6 +10,7 @@ int socket_memoria;
 int socket_filesystem;
 arch_config_kernel config_valores_kernel;
 
+//========================================================================================================================================
 int main(void)
 {
 	kernel_logger = log_create("/home/utnso/tp-2023-2c-KernelTitans/kernel/cfg/kernel.log", "kernel.log", 1, LOG_LEVEL_INFO);
@@ -19,33 +19,34 @@ int main(void)
 
 	log_info(kernel_logger, "Archivo de configuracion cargado \n");
 
-// INICIAR PLANIFICACIÓN //
     //inciar_planificacion()
     
-// CONEXION CPU //
+    //conexion con CPU
     socket_cpu_dispatch = crear_conexion(config_valores_kernel.ip_cpu, config_valores_kernel.puerto_cpu_dispatch);
 	socket_cpu_interrupt = crear_conexion(config_valores_kernel.ip_cpu, config_valores_kernel.puerto_cpu_interrupt);
 
     log_info(kernel_logger, "Kernel conectado con cpu \n");
 
-// CONEXION MEMORIA //
+    //conexion con memoria
     socket_memoria = crear_conexion(config_valores_kernel.ip_memoria, config_valores_kernel.puerto_memoria);
 
     log_info(kernel_logger, "Kernel conectado con memoria \n");
 
-// CONEXION FILE SYSTEM //
+   //conexion con filesystem
    socket_filesystem = crear_conexion(config_valores_kernel.ip_filesystem, config_valores_kernel.puerto_filesystem);
 
    log_info(kernel_logger, "Kernel conectado con filesystem \n");
 
-// INICIAR SERVIDOR //
+   //iniciamos el servidor
    server_fd = iniciar_servidor(config_valores_kernel.ip_kernel,config_valores_kernel.puerto_escucha);
 
    log_info(kernel_logger, "Servidor creado \n");
 
    log_info(kernel_logger, "Kernel listo para recibir al modulo cliente \n");
 
-   while(atender_clientes_kernel(server_fd));
+   while(atender_clientes_kernel(server_fd)); //creo que lo tenemos que hacer en un hilo
+
+   void finalizar_kernel();
 
    return EXIT_SUCCESS;
 
