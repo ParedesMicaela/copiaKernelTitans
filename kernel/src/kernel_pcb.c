0198d359787142e3c32ffcdda2bdc93db15f63cd
Paredes Micaela <miparedes@frba.utn.edu.ar>
2023-09-14 16:55:14 -0300

    FIFO corto plazo

--- kernel/src/kernel_pcb.c
+++ kernel/src/kernel_pcb.c
@@ -8,7 +8,7 @@ uint32_t indice_pid = 0;
 t_pcb *crear_pcb(int prioridad, int tam_swap) 
 {
     //esto lo ponemos aca para no tener que hacerlo en la funcion iniciar_proceso si total lo vamos a hacer siempre
-    t_pcb *pcb = malloc(sizeof(*pcb)); 
+    t_pcb *pcb = malloc(sizeof(*pcb)); //nota de Martín: este malloc después se libera en cpu cuando termina el proceso junto al pcb (posible memory leak?)
 
     //el indice lo vamos a estar modificando cada vez que tengamos que crear un pcb entonces conviene ponerlo como variable global
     //cosa que todos sabemos cuanto vale y no repetimos pid
@@ -72,49 +72,39 @@ void enviar_pcb_a_cpu(t_pcb* pcb_a_enviar)
     return;
 }
 
+//hay que ver bien esto porque le faltan cosas
+ char* recibir_contexto(t_pcb* proceso)
+ {
+    //esta funcion lo que hace es recibir un paquete, si ese paquete es un pcb lo abre y nos dice el motivo por el cual se devolvio
+    char* motivo_de_devolucion = NULL;
+    t_paquete* paquete = recibir_paquete(socket_cpu_dispatch);
+    void* stream = paquete->buffer->stream;
+    int program_counter =-1;
 
-void destruir_pcb (t_pcb* pcb){
-    pthread_mutex_lock(pcb_get_mutex(pcb));
-
-//ir agregando las cosas que sean necesarias eliminar
-
-    t_dictionary *archivosAbiertos = pcb->archivosAbiertos;
-    if (archivosAbiertos != NULL){
-        dictionary_destroy(archivosAbiertos);
+    //si lo que recibimos es en efecto un pcb, lo abrimos
+	if(paquete->codigo_operacion == PCB)
+	{
+		//nosotros solamente vamos a sacar el contexto
+        program_counter = sacar_entero_de_paquete(&stream);
+        //registros
+        motivo_de_devolucion = sacar_cadena_de_paquete(&stream);
+    }
+    else{
+        log_info(kernel_logger, "Falla al recibir PCB, se cierra el Kernel");
+        exit(1);
     }
 
-    t_registros_cpu *registros_cpu = pcb->registros_cpu.AX;
-    if (registros_cpu->AX != NULL){
-        free(pcb->registros_cpu.AX);
-    }
-     t_registros_cpu *registros_cpu = pcb->registros_cpu.BX;
-    if (registros_cpu->BX != NULL){
-        free(pcb->registros_cpu.BX);
-    }
-     t_registros_cpu *registros_cpu = pcb->registros_cpu.CX;
-    if (registros_cpu->CX != NULL){
-        free(pcb->registros_cpu.CX);
-    }
-     t_registros_cpu *registros_cpu = pcb->registros_cpu.DX;
-    if (registros_cpu->DX != NULL){
-        free(pcb->registros_cpu.DX);
-    }
-    //eliminar solo el diccionario sirve o también hay que eliminar sus elementos?
-    //en tal caso tendría que ser un dictionary_destroy_and_destroy_elements
-    pthread_mutex_unlock(pcb_get_mutex(pcb));
+	//actualizamos el pc y los registros
+	proceso->program_counter = program_counter;
+	//proceso->registros_cpu = registros;
 
-    pthread_mutex_destroy(pcb->mutex);
-    free(pcb->mutex); //libero el malloc de la linea 29
+    log_info(kernel_logger, "Recibi el pcb de la CPU con program counter = %d", program_counter);
 
-    free(pcb); // libero el malloc de la linea 11
+    //retornamos el motivo de devolucion para que ejecutar se encargue de manejarlo
+    return motivo_de_devolucion;
+ }
 
-}
-
-
-//get mutex pcb
-
-pthread_mutex_t* pcb_get_mutex(t_pcb* pcb)
+void eliminar_pcb(t_pcb* proceso)
 {
-    return pcb->mutex;
-}
 
+}
