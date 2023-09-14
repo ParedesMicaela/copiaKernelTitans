726b77eeefece5e71a99d95f95b1a37fa9cecda5
Nicolas Bru <nbru@frba.utn.edu.ar>
2023-09-14 19:22:43 +0000

    estructura del kernel_consola.c 1er version

---
+++ kernel/src/kernel_consola.c
@@ -0,0 +1,23 @@
+#include "kernel.h"
+
+//inicializar consola
+/*
+ como pingo inicializar una colsola interactiva 
+*/
+
+//inicializar proceso
+//INICIAR_PROCESO(PATH,SIZE,PRIORIDAD);
+
+/*
+-Ejecuta un nuevo proceso en base a un archivo dentro del file system de linux
+-Creación del proceso (PCB) y dejará el mismo en el estado NEW.
+*/
+
+//finalizar proceso
+//FINALIZAR_PROCESO(PID);
+
+/*
+-Finalizar un proceso que se encuentre dentro del sistema.
+-Realiza las mismas operaciones como si el proceso llegara a EXIT por sus caminos habituales
+-deberá liberar recursos, archivos y memoria.
+*/
