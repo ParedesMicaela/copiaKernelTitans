cf582191337cbb72e2e63663567c679153a4146e
Paredes Micaela <miparedes@frba.utn.edu.ar>
2023-09-17 16:10:08 -0300

    planificacion con hilos

--- kernel/src/kernel_planificacion.c
+++ kernel/src/kernel_planificacion.c
@@ -1,51 +1,3 @@
-// largo plazo
-
-/*
-
-Generarse la estructura PCB y asignar este PCB al estado NEW.
-
-
-En caso de que el grado máximo de multiprogramación lo permita
-    los procesos pasarán al estado READY
-    enviando un mensaje al módulo Memoria para que inicialice sus estructuras necesarias.
-
-La salida de NEW será mediante el algoritmo FIFO.
-
-
-Cuando se reciba un mensaje de CPU con motivo de finalizar el proceso
-    se deberá pasar al mismo al estado EXIT
-    liberar todos los recursos que tenga asignados
-    dar aviso al módulo Memoria para que éste libere sus estructuras.
-
-*/
-
-// corto plazo
-/*
-
-Los procesos que estén en estado READY serán planificados mediante uno de los siguientes algoritmos:
-    -FIFO
-    -Round Robin
-    -Prioridades (con desalojo)
-
-
-Una vez seleccionado el siguiente proceso a ejecutar:
-    -se lo transicionará al estado EXEC
-    -se enviará su Contexto de Ejecución al CPU a través del puerto de dispatch
-    -queda a la espera de recibir:
-        --contexto actualizado después de la ejecución
-        --un motivo de desalojo por el cual fue desplazado a manejar. 
-
-
-En caso que el algoritmo requiera desalojar al proceso en ejecución
-   - enviar interrupción a través de la conexión de interrupt para forzar el desalojo del mismo.
-
-
-Al recibir el Contexto de Ejecución del proceso en ejecución
-    -en caso de que el motivo de desalojo implique replanificar
-        --se seleccionará el siguiente proceso a ejecutar según indique el algoritmo. 
-            ---Durante este período la CPU se quedará esperando el nuevo contexto (es de esta parte o de todo el corto plazo???)
-*/
-
 #include "kernel.h"
 
 //=============================================== Variables Globales ========================================================
@@ -71,14 +23,18 @@ para acceder a las listas y sacarles el tamanio o agregar/eliminar procesos*/
 pthread_mutex_t mutex_ready;
 pthread_mutex_t mutex_exec;
 pthread_mutex_t mutex_exit;
+pthread_mutex_t mutex_new;
 
-
-sem_t gradoMultiprogramacion;
+sem_t grado_multiprogramacion;
 sem_t dispatchPermitido;
 //pthread_mutex_t mutexSocketMemoria; 
 //pthread_mutex_t mutexSocketFileSystem; los comento porque son terreno inexplorado por ahora
 sem_t semFRead;
 sem_t semFWrite;
+sem_t mutex_colas;
+
+sem_t hay_procesos_ready;
+
 bool fRead;
 bool fWrite;
 
@@ -101,58 +57,64 @@ void inicializar_semaforos(){
     int grado = config_get_int_value(config, "GRADO_MULTIPROGRAMACION_INI");
     pthread_mutex_init(&mutex_ready, NULL);
     pthread_mutex_init(&mutex_exec,NULL); 
+    pthread_mutex_init(&mutex_exit,NULL); 
+    pthread_mutex_init(&mutex_new,NULL); 
 
-    sem_init(&gradoMultiprogramacion, 0, grado);
+
+    sem_init(&grado_multiprogramacion, 0, grado);
+    sem_init (&(mutex_colas), 0, 1);
+    sem_init (&(hay_procesos_ready), 0, 0);
+
 }
 
 void planificador_largo_plazo()
 {
-    sem_wait (&gradoMultiprogramacion);
+    while(1){
+        //si el grado de multiprogramacion lo permite
+        sem_wait (&grado_multiprogramacion);
+
+        //elegimos el que va a pasar a ready, o sea el primero porque es FIFO
+        t_pcb* proceso_nuevo = obtener_siguiente_new();
+
+        //metemos el proceso en la cola de ready
+        meter_en_cola(proceso_nuevo,READY);
+
+        /*le avisamos al corto plazo que puede empezar a planificar. Aca solamente vamos a poner el proceso
+        en la cola de ready pero no vamos a elegir cual va a ejecutar el de corto plazo porque no hacemos eso
+        y le estamos robando el trabajo. Solamente vamos a poner el proceso en la cola de ready si el grado de 
+        multiprogramacion lo permite y despues que se arregle el corto plazo*/
+        sem_post (&hay_procesos_ready);  
+    } 
 }
 
-void planificador_corto_plazo(t_pcb* pcb)
+void planificador_corto_plazo()
 {
     while(1){
-    /*vamos a usar hilos para manejar toda la parte de ready execute y blocked porque sabemos que 
-    el planificador tiene que poder hacer varias cosas a la vez*/
-	pthread_create(&thread_ready, NULL, (void *)proceso_en_ready, pcb);
-	pthread_create(&thread_exec, NULL, (void *)proceso_en_execute, pcb);
-    //pthread_create(&thread_blocked, NULL, (void *)proceso_en_blocked, NULL);
+        sem_wait(&hay_procesos_ready);
 
-    //y le mando esto porque despues quiero que ellos solos me borren las porquerias que hacen
-	pthread_detach(thread_ready);
-	pthread_detach(thread_exec);
-	//pthread_detach(thread_blocked);
+        proceso_en_ready();
     }
 }
 
-void planificar (t_pcb* pcb)
-{
-    //un hilo para largo y uno para corto, hilo para consola, en bucles usamos join, detach en corto y largo
-    //
-    planificador_largo_plazo(pcb);
-    log_info(kernel_logger, "Iniciando planificador de largo plazo..\n");
-    planificador_corto_plazo(pcb);
-}
-
 //======================================================== Estados ==================================================================
-void proceso_en_ready(t_pcb* siguiente_proceso)
+void proceso_en_ready()
 {
-    while(1)
-    {
-        log_info(kernel_logger, "estoy en proceso ready\n");
+    log_info(kernel_logger, "estoy en proceso ready\n");
 
         //creamos un proceso, que va a ser el elegido por obtener_siguiente_ready
-        siguiente_proceso = obtener_siguiente_ready();
+        t_pcb* siguiente_proceso = obtener_siguiente_ready();
 
         //lo metemos en la cola de ready y avisamos que lo metimos ahi
+
+        log_info(kernel_logger, "PID: %d - Estado Anterior: %s\n", siguiente_proceso->pid,siguiente_proceso->estado_pcb);
+
+
         meter_en_cola(siguiente_proceso,READY);
         mostrar_lista_pcb(READY);
 
         log_info(kernel_logger, "PID[%d] ingresando a EXEC\n", siguiente_proceso->pid);
 
         proceso_en_execute(siguiente_proceso);
-    }
 }
 
 void proceso_en_execute(t_pcb* proceso_seleccionado)
@@ -203,11 +165,25 @@ void proceso_en_exit(t_pcb* proceso){
 
  	eliminar_pcb(proceso);
  	free(proceso);
- 	sem_post(&gradoMultiprogramacion);
+ 	sem_post(&grado_multiprogramacion);
 	}
 }
 
 //======================================================== Algoritmos ==================================================================
+
+t_pcb* obtener_siguiente_new()
+{
+    //mostramos los que estan en new
+ 	mostrar_lista_pcb(cola_NEW);
+
+    //los procesos salen de new a ready por FIFO entonces usamos la misma funcion que obtener_siguiente_FIFO
+	pthread_mutex_lock(&mutex_new);
+	t_pcb* proceso_seleccionado = list_remove(dictionary_int_get(diccionario_colas, NEW), 0);
+	pthread_mutex_unlock(&mutex_new);
+
+ 	log_info(kernel_logger, "PID[%d] sale de NEW para planificacion \n", proceso_seleccionado->pid);
+	return proceso_seleccionado;
+}
 
 /*esta funcion la voy a poner para que me haga todo el calculo de que proceso deberia ir primero dependiendo
 del algoritmo que estoy usando. Y la pongo aca porque es mas facil solamente poner una linea en la parte de 
@@ -247,7 +223,7 @@ t_pcb* obtener_siguiente_ready()
 
  	/*vemos si todavia hay procesos en ready y si el grado de multiprogramacion me permite ejecutar 
     los procesos que estoy ejecutando justo ahora con un proceso mas*/
- 	if (tamanio_cola_ready > 0 && ejecutando < grado){
+ 	if (tamanio_cola_ready >= 0 && ejecutando < grado){
  		switch(algoritmo_elegido){
  		case FIFO:
  			proceso_seleccionado = obtener_siguiente_FIFO();
@@ -270,13 +246,11 @@ algoritmo obtener_algoritmo(){
     char* algoritmo_actual = NULL;
  	algoritmo_actual = config_valores_kernel.algoritmo_planificacion;
 
-    log_info(kernel_logger, "El algoritmo actual de planificacion es %s \n",algoritmo_actual);
-
  	    //FIFO
  	if (strcmp(algoritmo_actual,"FIFO") == 0)
  	{
  		switcher = FIFO;
- 		//log_info(kernel_logger, "El algoritmo de planificacion elegido es FIFO \n");
+ 		log_info(kernel_logger, "El algoritmo de planificacion elegido es FIFO \n");
  	}
  	    //PRIORIDADES
  	if (strcmp(algoritmo_actual,"PRIORIDADES") == 0)
@@ -302,12 +276,19 @@ t_pcb* obtener_siguiente_FIFO()
 
  	log_info(kernel_logger, "PID[%d] sale de READY por planificacion FIFO \n", proceso_seleccionado->pid);
 	return proceso_seleccionado;
-};
+}
 
 t_pcb* obtener_siguiente_PRIORIDADES()
 {
+    ///
+    printf("<3");
+}
 
-};
+t_pcb* obtener_siguiente_RR() // tener en cuenta como implementar con quantum
+{
+   ///
+   printf("<3");
+}
 
 //=================================================== Diccionarios y Colas ==================================================================
 void inicializar_diccionarios()
@@ -350,14 +331,20 @@ void meter_en_cola(t_pcb* pcb, estado ESTADO)
     pasandole el estado del proceso (key) nos va a devolver la cola en la que esta*/
     t_list* cola = dictionary_int_get(diccionario_colas, pcb->estado_pcb);
 
+    //hay que poner un if por si la cola esta vacia jejej
+
+    log_info(kernel_logger, " Cola %s\n",(char *)dictionary_int_get(diccionario_estados, cola));
+
     //recorremos la cola y buscamos el pid del pcb
     for(int i=0;i<list_size(cola) ;i++)
         {
             if(pcb->pid == ((t_pcb*) list_get(cola, i))->pid)
             {
+                sem_wait(&(mutex_colas));
                 /*y cuando los encontramos lo vamos a sacar porque va a cambiar de estado entonces ya
                 no lo quiero en esa cola*/
-                list_remove(cola, i);
+                list_remove(cola, i); //aca me sale error
+                sem_post(&(mutex_colas));
             }
       }
 
@@ -368,7 +355,9 @@ void meter_en_cola(t_pcb* pcb, estado ESTADO)
     pcb->estado_pcb= ESTADO;
 
     //finalmente lo agregamos a la cola de nuestro nuevo estado
+    sem_wait(&(mutex_colas));
     list_add(dictionary_int_get(diccionario_colas, ESTADO), pcb);
+    sem_post(&(mutex_colas));
     log_info(kernel_logger, "PID: %d - Estado Anterior: %s - Estado Actual %s\n", pcb->pid,(char *)dictionary_int_get(diccionario_estados, estado_viejo),(char *)dictionary_int_get(diccionario_estados, ESTADO));
 }
 
@@ -393,4 +382,53 @@ void mostrar_lista_pcb(t_list* cola){
 	//mostramos la lista con los pids en la cola dada
 	log_info(kernel_logger, " Cola %s %s : [%s]\n",(char *)dictionary_int_get(diccionario_estados, cola),config_valores_kernel.algoritmo_planificacion, pids);
 	free(pids);
- }
+}
+
+
+// largo plazo
+
+/*
+
+Generarse la estructura PCB y asignar este PCB al estado NEW.
+
+
+En caso de que el grado máximo de multiprogramación lo permita
+    los procesos pasarán al estado READY
+    enviando un mensaje al módulo Memoria para que inicialice sus estructuras necesarias.
+
+La salida de NEW será mediante el algoritmo FIFO.
+
+
+Cuando se reciba un mensaje de CPU con motivo de finalizar el proceso
+    se deberá pasar al mismo al estado EXIT
+    liberar todos los recursos que tenga asignados
+    dar aviso al módulo Memoria para que éste libere sus estructuras.
+
+*/
+
+// corto plazo
+/*
+
+Los procesos que estén en estado READY serán planificados mediante uno de los siguientes algoritmos:
+    -FIFO
+    -Round Robin
+    -Prioridades (con desalojo)
+
+
+Una vez seleccionado el siguiente proceso a ejecutar:
+    -se lo transicionará al estado EXEC
+    -se enviará su Contexto de Ejecución al CPU a través del puerto de dispatch
+    -queda a la espera de recibir:
+        --contexto actualizado después de la ejecución
+        --un motivo de desalojo por el cual fue desplazado a manejar. 
+
+
+En caso que el algoritmo requiera desalojar al proceso en ejecución
+   - enviar interrupción a través de la conexión de interrupt para forzar el desalojo del mismo.
+
+
+Al recibir el Contexto de Ejecución del proceso en ejecución
+    -en caso de que el motivo de desalojo implique replanificar
+        --se seleccionará el siguiente proceso a ejecutar según indique el algoritmo. 
+            ---Durante este período la CPU se quedará esperando el nuevo contexto (es de esta parte o de todo el corto plazo???)
+*/
