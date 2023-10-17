553d9bb6519953976299d5bdf85460e99ec03c5f
Paredes Micaela <miparedes@frba.utn.edu.ar>
2023-10-17 18:55:03 -0300

    la memoria manda correctamente las instrucciones

--- memoria/src/memoria_utils.c
+++ memoria/src/memoria_utils.c
@@ -1,5 +1,8 @@
 #include "memoria.h"
 
+t_list* lista_procesos;
+
+
 // CONFIGURACION //
 void cargar_configuracion(char* path){
 	
@@ -40,6 +43,9 @@ void manejo_conexiones(void* socket_cliente)
 {
 	int cliente = *(int*)socket_cliente;
 	int posicion_pedida = 0;
+	int pid_proceso = 0;
+	char* path_asignado = NULL;
+
 	while(1){
 	t_paquete* paquete = recibir_paquete(cliente);
     void* stream = paquete->buffer->stream;
@@ -55,36 +61,59 @@ void manejo_conexiones(void* socket_cliente)
 		enviar_paquete_handshake(cliente);
 		break;
 
-	case RECIBIR_PATH:
+	case MANDAR_INSTRUCCIONES:
+
+		//la cpu nos manda el program counter y el pid del proceso que recibio para ejecutar
+		posicion_pedida = sacar_entero_de_paquete(&stream);
+		pid_proceso = sacar_entero_de_paquete(&stream);
+
+		//aca vamos a buscar el proceso con el pid que recibimos y obtener el path_asignado
+		path_asignado = buscar_path_proceso(pid_proceso); 
+
+		//mandamos directamente el path del proceso porque ahi ya voy a tener las instrucciones leidas y cargadas
+		enviar_paquete_instrucciones(cliente, path_asignado, posicion_pedida);
+		break;
+
+	case CREACION_ESTRUCTURAS_MEMORIA:
+
+    	t_proceso_en_memoria* proceso = malloc(sizeof(t_proceso_en_memoria));
+		
+		pid_proceso = sacar_entero_de_paquete(&stream);
+		int tam_swap_pid = sacar_entero_de_paquete(&stream);
 
 		//del kernel va a recibir un path que va a tener que leer para pasarle a la cpu la instruccion
 		char* path_recibido = sacar_cadena_de_paquete(&stream);
+
+		//aca cargamos los datos en nuestra estructura proceso
+		proceso->pid = pid_proceso;
+		proceso->tam_swap = tam_swap_pid;
 
 		/*el path va a ser distinto al del archivo de config entonces lo tengo que modificar
 	    no lo pongo en MANDAR_INSTRUCCIONES porque el kernel no me pide que le lea la instruccion, eso me lo pide la cpu
 		el kernel me dice solamente el path que necesito leerle a la cpu*/
 		config_valores_memoria.path_instrucciones = path_recibido;
-		log_info(memoria_logger, "PATH recibido: %s", path_recibido);
-		break;
 
-	case MANDAR_INSTRUCCIONES:
-		//leemos el archivo de pseudo codigo del path de la config  y lo metemos en una cadena TODO JUNTO
-		char* instrucciones_leidas = leer_archivo_instrucciones(config_valores_memoria.path_instrucciones);
+		/*decidi leer el path que me manda el kernel al principio, y guardar las instrucciones dentro de mi
+		estructura proceso para no tener que leer el archivo cada vez que tengo que mandar instrucciones. Si lo 
+		hago solamente al principio y guardo las instrucciones leidas en el path_proceso del proceso, me ahorro
+		tener que hacerlo todo el tiempo y ademas ya lo tengo todo leido y preparado para solamente devolver el
+		indice (program_counter) que me pide*/
+		char* instrucciones_leidas = leer_archivo_instrucciones(path_recibido);
+		
+		proceso->path_proceso = strdup(instrucciones_leidas);
 
-		//necesito sacar del paquete la posicion de program_counter
-		posicion_pedida = sacar_entero_de_paquete(&stream);
-		enviar_paquete_instrucciones(cliente, instrucciones_leidas, posicion_pedida);
-		break;
+		log_info(memoria_logger, "PATH recibido: %s", config_valores_memoria.path_instrucciones );
 
-	case CREACION_ESTRUCTURAS_MEMORIA:
-		int pid_proceso = sacar_entero_de_paquete(&stream);
-		int tam_swap_pid = sacar_entero_de_paquete(&stream);
+		//decidi hace una lista para buscar mas facil los datos de cada proceso
+		list_add(lista_procesos, (void*)proceso);
 
 		log_info(memoria_logger,"Recibi pedido de creacion de estructuras en memoria\n");
 		//crear_tablas_paginas_proceso(pid_proceso, tam_swap_pid);
         int ok_creacion = 1;
         send(cliente, &ok_creacion, sizeof(int), 0);
 		log_info(memoria_logger,"Estructuras creadas en memoria kernel-kyunn\n");
+		free(instrucciones_leidas);
+		free(path_recibido);
 		break;
 
 	case FINALIZAR_EN_MEMORIA:
@@ -102,5 +131,4 @@ void manejo_conexiones(void* socket_cliente)
 	eliminar_paquete(paquete);
 	}
 }
-
 
